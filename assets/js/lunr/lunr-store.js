70881a1cc5bf78be86fb8b725cea5b46a29ee1e2
kendallstarcevich <kendallstarcevich@users.noreply.github.com>
2025-08-28 15:45:10 +0000

    deploy: 91ec7adb4a2fc04e8b1ffd585bb173f907d4654b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,121 @@
+var store = [{
+        "title": "Effects on Employment Rates & Income",
+        "excerpt":"A statistics project analyzing data based on employment rates and income of different college majors after graduation. EFFECTS ON EMPLOYMENT RATES &amp; INCOME: MAJOR CATEGORY, GENDER, GRADUATE DEGREE Below is my Final Project Report for my intro to R and SAS Statistics course. This project used SAS and R programming...","categories": ["work"],
+        "tags": ["R","SAS"],
+        "url": "/work/2023/12/12/stat040.html",
+        "teaser": "/assets/images/graph.png"
+      },{
+        "title": "OOP Quiz GUI",
+        "excerpt":"My final Object Oriented Programming Java project where I created a Quiz GUI. OOP Quiz GUI GitHub Repo for this project The README in my GitHub repo above shows how to execute this program. The program allows you to create a custom “quiz” game, where you personalize your questions and...","categories": ["work"],
+        "tags": ["OOP","Java"],
+        "url": "/work/2023/12/15/OOPFInalProject.html",
+        "teaser": "/assets/images/OOP1.png"
+      },{
+        "title": "Street Sweeper World Projects",
+        "excerpt":"Three AI bots that interact with similated street network enviromnment to clean streets. Project 1: Street Sweeper World I was given a reflex agent using the OSMnx Library to retreieve real map data from OpenStreetMap that can perceive its battery life, meters cleaned, and current location, as well as any...","categories": ["work"],
+        "tags": ["AI","Python"],
+        "url": "/work/2024/02/13/StreetSweeperWorldProjects.html",
+        "teaser": "/assets/images/streetsweeper.jpg"
+      },{
+        "title": "Q-Learning for Gymnasium Environments",
+        "excerpt":"Q-Learning Algorithm agent in Gymnasium Enviornments Project 4: Q-LEarning for Gymnasium Environments I completed a lab in class to get a Q-Learning algorithm working in the Gymnasium Frozen Lake Environment. For this project, I had to implement it in another Gymnasium environment and evaluate how well my Q-Learning agent performed...","categories": ["work"],
+        "tags": ["AI","Python"],
+        "url": "/work/2024/04/11/Q-LearningForGymnasiumEnvironments.html",
+        "teaser": "/assets/images/taxi.jpg"
+      },{
+        "title": "Bootstrap Portfolio Website",
+        "excerpt":"   A locally hosted Portfolio Website powered by HTML, Javascript, CSS, and Bootstrap.    Bootstrap Portfolio Project           ","categories": ["work"],
+        "tags": ["Bootstrap","Development"],
+        "url": "/work/2024/04/17/portfolio.html",
+        "teaser": "/assets/images/portfolio.png"
+      },{
+        "title": "Constraint Satisfaction Problem (CSP)",
+        "excerpt":"A real-life solution to a CSP to create a 4-year plan for a student. Four-Year Academic Plan as a CSP As part of I implemented a CSP using the python-constraints package to create a 4-year plan that shows which courses to take in each semester as a real Drake Computer...","categories": ["work"],
+        "tags": ["Python"],
+        "url": "/work/2024/04/30/ConstraintSatisfactionProblem.html",
+        "teaser": "/assets/images/CSP.jpg"
+      },{
+        "title": "MVC Men’s & Women’s Basketball Database",
+        "excerpt":"SQL powered data analysis regarding MVC institution’s financial impacts on both the men’s and the women’s basketball teams. Database Project: MVC Men’s &amp; Women’s Basketball Below is the report for my Final Project for my database management course. We used SQL to analyze to understand each MVC institution’s financial impacts...","categories": ["work"],
+        "tags": ["SQL","PowerBI"],
+        "url": "/work/2024/05/04/IS160.html",
+        "teaser": "/assets/images/valley.jpg"
+      },{
+        "title": "Retrieval Augmented Generation Chatbot",
+        "excerpt":"Improvement and customization of RAG/Semantic Search Chatbot RAG Chatbot In my Artificial Intelligence programming course, we worked through creating a chatbot using RAG and semantic search. This chatbot has an input limit of 128, which was not effective. My assignment was to implement a different model, specifically the Google Flan-t5-base,...","categories": ["work"],
+        "tags": ["AI","Python"],
+        "url": "/work/2024/05/09/RAGChatbot.html",
+        "teaser": "/assets/images/chatbot.jpg"
+      },{
+        "title": "Event Calendar Software Solution",
+        "excerpt":"Completed the entire software development process in a semester long effort to make a University event calendar prototype. Drake Event Calendar App Completed Need Finding, Ideation, Personas and Storyboard, Lo-Fi Prototyping and Evals, Hi-Fi Protoype and Expert Review, and a Usability Study for an Event Calendar App. Watch a 5-minute...","categories": ["work"],
+        "tags": ["Development","Figma"],
+        "url": "/work/2024/05/10/HCI.html",
+        "teaser": "/assets/images/app.png"
+      },{
+        "title": "Insure Your Future Internship Program",
+        "excerpt":"My experience with Iowa Economic Authority’s Insure Your Future Internship Program. IYF Internship Program In addition to my Internship at Wellmark, I participated in the Iowa Economic Authority’s Insure Your Future Internship Program for first and second year college interns in different financial and insurance companies across Iowa. I describe...","categories": ["work"],
+        "tags": ["Internships"],
+        "url": "/work/2024/05/20/InsureYourFuture.html",
+        "teaser": "/assets/images/iyf.png"
+      },{
+        "title": "Wellmark Health Services Operations & Systems Internship",
+        "excerpt":"My projects and impact throughout my internship of Summer 2024. Final Presentation Here is my final presentation that I gave at the conclusion of my internship in front of the company execs. It touches on some of the most important things I did over the summer, including improving a daily...","categories": ["work"],
+        "tags": ["Internships","SQL"],
+        "url": "/work/2024/05/20/WellmarkInternship.html",
+        "teaser": "/assets/images/wellmark.png"
+      },{
+        "title": "Drake Insurance Innovation Lab Internship",
+        "excerpt":"My experience with Drake Insurance Innovation Lab Internship. Drake Insurance Innovation Lab Internship I am an intern at the Kelley Center for Insurance Innovation in the Innovation Lab. • As the group’s Data Scientist, set to develop strategic solutions for Wellabe’s new rewards program launch, delivering recommendations to senior executives....","categories": ["work"],
+        "tags": ["Internships"],
+        "url": "/work/2024/09/05/DIIL.html",
+        "teaser": "/assets/images/drake.png"
+      },{
+        "title": "32-bit ALU in Logism",
+        "excerpt":"A 32-bit ALU built in Logism that allows for adding, subtracting, and, or, not, xor, and slt operations. Tests Included. Computer Organization Project: 32-bit ALU Design This project was part of a course focused on computer organization and architecture, covering internal data representation, assembly language, memory addressing, and digital circuit...","categories": ["work"],
+        "tags": ["MIPS"],
+        "url": "/work/2024/10/17/32-bitALU.html",
+        "teaser": "/assets/images/alu.jpeg"
+      },{
+        "title": "Graduate Admission Prediction Project",
+        "excerpt":"A Machine Learning project consisting of kNN, Decision Tree, and Random Forest models that predict the chance of graduate college admission. Graduate Admission Prediction Project This project analyzes a Kaggle dataset containing 400 university admission profiles, featuring GRE scores, TOEFL scores, university ratings, statement of purpose (SOP) and recommendation letter...","categories": ["work"],
+        "tags": ["Python","ML"],
+        "url": "/work/2024/11/04/CS167P1.html",
+        "teaser": "/assets/images/ML.jpg"
+      },{
+        "title": "MIPS CPU Design Project",
+        "excerpt":"A MIPS CPU in Logisim to support a wider range of instructions, including arithmetic, logical, and set-on-less-than operations Computer Organization Project: MIPS CPU Design Project For this project, I extended a partial MIPS CPU in Logisim to support a wider range of instructions, including arithmetic, logical, and set-on-less-than operations (add,...","categories": ["work"],
+        "tags": ["MIPS"],
+        "url": "/work/2024/11/05/CPU.html",
+        "teaser": "/assets/images/CPUthumb.jpeg"
+      },{
+        "title": "Fine-Grained Visual Categorization (FGVC) of Bird Species",
+        "excerpt":"Comparison of convolutional neural network (CNN) models’ abilities to accurately classify bird species Fine-Grained Visual Categorization (FGVC) of Bird Species In this deep learning project, I developed and evaluated convolutional neural network (CNN) models to accurately classify bird species from images using the Bird Species Dataset, which includes 25 different...","categories": ["work"],
+        "tags": ["Python","ML"],
+        "url": "/work/2024/12/10/CS167P2.html",
+        "teaser": "/assets/images/birdthumb.jpg"
+      },{
+        "title": "Airbnb Seattle Web App (Flask + AWS)",
+        "excerpt":"A full-stack Flask web application for browsing and managing Seattle Airbnb listings and reviews. Airbnb Seattle Web App Project A full-stack Flask web application for browsing and managing Seattle Airbnb listings and reviews. It integrates a MySQL database (AWS RDS) for listings/reviews and DynamoDB for user authentication. Users can sign...","categories": ["work"],
+        "tags": ["AWS","Python","Flask","Development","SQL"],
+        "url": "/work/2025/04/17/CS178P1.html",
+        "teaser": "/assets/images/airbnbthumb.png"
+      },{
+        "title": "KMEANS Consulting for Wellabe (DIIL)",
+        "excerpt":"A project using k-means clustering to segment Wellabe customers. Wellabe Consultation Project This project was a part of my experience in the Drake Insurance Innovation Lab Objective: Optimize engagement personalization using data analysis. Considering how important personalization is to an upcoming Wellabe Rewards Program, identify how I would segment their...","categories": ["work"],
+        "tags": ["Internships","SQL","Python","Snowflake"],
+        "url": "/work/2025/04/24/Wellabe.html",
+        "teaser": "/assets/images/Wellabethumb.png"
+      },{
+        "title": "HIPAAi: Secure HIPAA Compliant AI Chatbot",
+        "excerpt":"AI Capstone Project that creates a HIPAA Compliant AI Chatbot using Langchain. AI Capstone Secure HIPAA Compliant AI Chatbot (HIPAAi) My Capstone Group developed a secure, role-based AI chatbot tailored for the health insurance industry to theoretically ensure compliance with HIPAA regulations. The chatbot uses a custom dataset and restricts...","categories": ["work"],
+        "tags": ["AI","Python"],
+        "url": "/work/2025/05/08/AICap.html",
+        "teaser": "/assets/images/hipaathumb.png"
+      },{
+        "title": "Amazon Cognito Demo",
+        "excerpt":"A tutorial that will walk through a step-by-step demo that creates a simple locally hosted web app with an Amazon Cognito powered login/signup. Amazon Cognito Demo I taught myself Amazon Cognito, an AWS User Authentication and Access Control service. I created a tutorial that will walk through a step-by-step demo...","categories": ["work"],
+        "tags": ["AWS","Development"],
+        "url": "/work/2025/05/12/CS178P2.html",
+        "teaser": "/assets/images/cognito.jpeg"
+      }]
